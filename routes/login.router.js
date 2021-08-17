d38e36465b28d26d3406d210c926d6ebad75da11
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-08-17 23:59:29 +0530

    refactor

--- routes/login.router.js
+++ routes/login.router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 const { User } = require("../models/user.model");
